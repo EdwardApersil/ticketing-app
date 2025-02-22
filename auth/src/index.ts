90e77be39096e939b57b014158e2740c952e3795
Edward Apersil <64957496+EdwardApersil@users.noreply.github.com>
2025-02-22 00:39:27 -0800

    Add user authentication routes and error handling middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,20 @@
 import express from 'express'
 import {json} from 'body-parser'
 
+import { currentUserRouter } from './routes/current-user'
+import { signupRouter } from './routes/signup'
+import { signinRouter } from './routes/signin'
+import { signoutRouter } from './routes/signout'
+import { errorHandler } from './middlewares/error-handler'
+
 const app = express()
 app.use(json())
+
+app.use(currentUserRouter)
+app.use(signupRouter)
+app.use(signinRouter)
+app.use(signoutRouter)
+app.use(errorHandler)
 
 app.get('/api/users/currentuser', (req, res) => {
     res.send('Hello Edward')
